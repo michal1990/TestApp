8fc0348739c087c2dd862ba713928a2388d5a2f1
Michał Radtke <michal.radtke@gmail.com>
2020-07-28 17:54:35 +0200

    Create list fragment with recycler view, adapter and view item.

---
+++ app/src/main/java/pl/mradtke/testapp/list/UserListAdapter.kt
@@ -0,0 +1,45 @@
+package pl.mradtke.testapp.list
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import pl.mradtke.model.ui.UserItem
+import pl.mradtke.testapp.databinding.ItemListUserBinding
+
+/**
+ * User list adapter. Data loading is supported by diff util.
+ *
+ * @author Michał Radtke
+ * @version 28.07.2020
+ */
+class UserListAdapter : ListAdapter<UserItem, UserListViewHolder>(Companion) {
+
+    companion object : DiffUtil.ItemCallback<UserItem>() {
+        override fun areItemsTheSame(oldItem: UserItem, newItem: UserItem): Boolean = oldItem === newItem
+        override fun areContentsTheSame(oldItem: UserItem, newItem: UserItem): Boolean = oldItem.username == newItem.username
+    }
+
+    private var onItemClickListener: ((UserItem) -> Unit)? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserListViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = ItemListUserBinding.inflate(layoutInflater, parent, false)
+
+        return UserListViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: UserListViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.binding.user = item
+        holder.binding.root.setOnClickListener { onItemClickListener?.invoke(item) }
+        holder.binding.executePendingBindings()
+    }
+
+    /**
+     * Set item click listener.
+     */
+    fun setOnItemClickListener(listener: ((UserItem) -> Unit)) {
+        onItemClickListener = listener
+    }
+}
--- model/src/main/java/pl/mradtke/model/ui/UserItem.kt
+++ model/src/main/java/pl/mradtke/model/ui/UserItem.kt
@@ -1,10 +1,14 @@
 package pl.mradtke.model.ui
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
 /**
  * @author Michał Radtke
  * @version 27.07.2020
  */
+@Parcelize
 data class UserItem(
         val username: String,
         val userUrl: String,
-        val avatarUrl: String)
+        val avatarUrl: String) : Parcelable
